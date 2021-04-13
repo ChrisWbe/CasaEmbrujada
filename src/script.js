5a4bc8d1b27ea012dc605f66c0e660902c619399
Christian Quintero <chriswbe1993@gmail.com>
2021-04-13 15:36:37 -0500

    Geometrias, Materiales, Luces, Fog

--- src/script.js
+++ src/script.js
@@ -23,13 +23,78 @@ const textureLoader = new THREE.TextureLoader()
 /**
  * House
  */
-// Temporary sphere
-const sphere = new THREE.Mesh(
-    new THREE.SphereGeometry(1, 32, 32),
-    new THREE.MeshStandardMaterial({ roughness: 0.7 })
+//House container
+const house = new THREE.Group()
+scene.add(house)
+
+const walls = new THREE.Mesh(
+    new THREE.BoxGeometry(4,2.5,4),
+    new THREE.MeshStandardMaterial({color:"#ac8e82"})
 )
-sphere.position.y = 1
-scene.add(sphere)
+walls.position.y=1.25
+house.add(walls)
+
+//Roof - Techo
+const roof = new THREE.Mesh(
+    new THREE.ConeGeometry(3.5,1,4),
+    new THREE.MeshStandardMaterial({color:'#b35f45'})
+)
+roof.position.y = 2.5+0.5 //paredes tienen altura de 2.5 y la altura del techo es de 1
+roof.rotation.y = Math.PI*0.25
+house.add(roof)
+
+//Door
+const door = new THREE.Mesh(
+    new THREE.PlaneGeometry(2,2),
+    new THREE.MeshStandardMaterial({color:'#aa7b7b'})
+)
+door.position.y =1
+door.position.z = 2 +0.01
+house.add(door)
+
+//Bushes - Arbustos
+const bushGeometry = new THREE.SphereGeometry(1,16,16)
+const bushMaterial = new THREE.MeshStandardMaterial({color:'#89c854'})
+const bush1 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush1.scale.set(0.5, 0.5, 0.5)
+bush1.position.set(0.8, 0.2, 2.2)
+const bush2 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush2.scale.set(0.25, 0.25, 0.25)
+bush2.position.set(1.4, 0.1, 2.1)
+const bush3 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush3.scale.set(0.4, 0.4, 0.4)
+bush3.position.set(- 0.8, 0.1, 2.2)
+const bush4 = new THREE.Mesh(bushGeometry, bushMaterial)
+bush4.scale.set(0.15, 0.15, 0.15)
+bush4.position.set(- 1, 0.05, 2.6)
+house.add(bush1, bush2, bush3, bush4)
+
+
+//Contenedor Tumbas
+//Graves - Tumbas
+const graves = new THREE.Group()
+scene.add(graves)
+const graveGeometry = new THREE.BoxGeometry(0.6,0.8,0.2)
+const graveMaterial = new THREE.MeshStandardMaterial({color:'#b2b6b1'})
+for(let i = 0; i<50; i++){
+    const angle = Math.random()*Math.PI*2 // angulo aleatorio
+    const radius = 3 + Math.random() * 6 // radio aleatorio
+    const x = Math.cos(angle) * radius
+    const z = Math.sin(angle) * radius
+    
+    //Se crea la tumba
+    const grave = new THREE.Mesh(graveGeometry, graveMaterial)
+
+    //Position
+    grave.position.set(x,0.3,z)
+
+    //Rotacion
+    grave.rotation.z = (Math.random()-0.5)*0.4
+    grave.rotation.y = (Math.random()-0.5)*0.4
+
+    //Se añade la tumba al contenedor
+    graves.add(grave)
+}
 
 // Floor
 const floor = new THREE.Mesh(
@@ -44,18 +109,30 @@ scene.add(floor)
  * Lights
  */
 // Ambient light
-const ambientLight = new THREE.AmbientLight('#ffffff', 0.5)
+const ambientLight = new THREE.AmbientLight('#b9d5ff', 0.12)
 gui.add(ambientLight, 'intensity').min(0).max(1).step(0.001)
 scene.add(ambientLight)
 
 // Directional light
-const moonLight = new THREE.DirectionalLight('#ffffff', 0.5)
+const moonLight = new THREE.DirectionalLight('#b9d5ff', 0.12)
 moonLight.position.set(4, 5, - 2)
 gui.add(moonLight, 'intensity').min(0).max(1).step(0.001)
 gui.add(moonLight.position, 'x').min(- 5).max(5).step(0.001)
 gui.add(moonLight.position, 'y').min(- 5).max(5).step(0.001)
 gui.add(moonLight.position, 'z').min(- 5).max(5).step(0.001)
 scene.add(moonLight)
+
+//Door Light
+const doorLight = new THREE.PointLight('#ff7d46',1,7)
+doorLight.position.set(0, 2.2,2.7)
+house.add(doorLight)
+
+
+/**
+ * Fog - Niebla
+ */
+const fog = new THREE.Fog('#262837', 1,15)
+scene.fog = fog
 
 /**
  * Sizes
@@ -102,6 +179,7 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setClearColor('#262837')
 
 /**
  * Animate
